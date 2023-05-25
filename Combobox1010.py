e155e75ce147e4d91a3b5b496960576ea2dc4a2a
Vladislav Sicotorschi <vvladic123@gmail.com>
2023-05-25 10:21:05 -0700

    yes

--- Combobox1010.py
+++ Combobox1010.py
@@ -12,3 +12,12 @@ def onSelect(event):
 #create root window
 root = tk.Tk()
 root.title("Combobox Example")
+
+#create array list for items
+items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
+
+#create combo box object and input values from items array
+comboBox = ttk.Combobox(root, values=items)
+
+#bind combo box object to the selector event
+comboBox.bind("<<ComboboxSelected>>", onSelect)
